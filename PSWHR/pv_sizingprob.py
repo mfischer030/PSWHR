23e3d46f85a47420fa9b46d4ba1ade8ec1ede309
GabrieleHu <125606357+GabrieleHu@users.noreply.github.com>
2024-02-08 09:32:42 +0100

    Update pv_sizingprob.py
    Comments from Gabriele are provided. 
    to find the comments, please search for 
    #Gabriele
    in the document. 
    No modifications to the code were made

--- PSWHR/pv_sizingprob.py
+++ PSWHR/pv_sizingprob.py
@@ -40,7 +40,7 @@ k    = 1.4                               # Ratio cp/cv [-]
 R_H2 = 4.1242                            # Individual Gas constant H2 [kJ/kg*K]
 HHV  = 39.39*3600*10**3                  # Higher heating value of H2 in [J/kg]
 
-project_lifetime     = 25        # in [y]
+project_lifetime     = 25        # in [y] #Gabriele: do you have a reference for this value by chance?
 annual_interest_rate = 0.04      # Discount rate, as encouraged by EU, from Rox
 
 
@@ -59,40 +59,40 @@ deltat = 3600                                      # time step (s)
 eta = {'PV': 0.21,'ELY': 0.6, 'C': 0.7526, 'TANK': 1, 'FC': 0.5}
 
 # Lifetime of the components in [years]
-life = {'PV': 25,'ELY': 20, 'C': 20, 'TANK': 35, 'FC': 20}
+life = {'PV': 25,'ELY': 20, 'C': 20, 'TANK': 35, 'FC': 20} #Gabriele: do you have references for these values? Can you please check the lifetime values for ely and FC
 
 # Unit prices of components / capital costs in [€/W] => from Roxanne
 UP = {
     'PV': 0.8,          # Unit price PV in [€/W]
     'ELY': 1.1,         # Unit price electrolyser [€/W]
     'C': 0.05,          # CHECK FOR DATA
-    'TANK': 0.1644/HHV, # Unit price hydrogen storage [EUR/J], 1644 [EUR/kgH2]
+    'TANK': 0.1644/HHV, # Unit price hydrogen storage [EUR/J], 1644 [EUR/kgH2]  
     'FC': 1,            # CHECK FOR DATA 
     'HP': 0.0576,       # Unit price heat pump
 }
 
 # Daily energy demand in [Wh] - P_demand is in [W]
-E_demand_day = sum(P_demand)/(4*days)
+E_demand_day = sum(P_demand)/(4*days) #Gabriele: this is [Wh], correct?
 # Maximal daily power demand in [W]
 P_peak_max = np.max(P_demand)
 
 # Defining maximal SIZES of the components-------------------------------------
 # maximum PV size needs to be defined to define the upper bound of the design variable.
-# Area_PV_max  = P_peak_max / (eta['PV'] * np.mean(irradiance)) # Maximum PV area [m2]
+# Area_PV_max  = P_peak_max / (eta['PV'] * np.mean(irradiance)) # Maximum PV area [m2] 
 Area_PV_max  = 10000 # Maximum PV area [m2]
 
-# Electrolyser maximal nominal power (W) very large (not realistic)  
+# Electrolyser maximal nominal power (W) very large (not realistic)
 S_ELY_max = 500*1000                         # Maximal Electrolyzer size in [W]
 
-# Calculating the spezific work of the compresso, from Minutillo et al. 2021
+# Calculating the spezific work of the compressor, from Minutillo et al. 2021
 # (Analyzing the levelized cost of hydrogen eq 1+2) => from Roxanne 
-T_in_H2 = 70 + 273.15                       # H2 temperature (=T_cat=T_an) [K]
-p_out   = 820                               # Compressor outlet pressure [bar]
-p_in    = 50                                # Compressor inlet pressure [bar]
+T_in_H2 = 70 + 273.15                       # H2 temperature (=T_cat=T_an) [K] #Gabriele: this could probably be 65 degC, see for example: https://www.sciencedirect.com/science/article/pii/S0360319923015410
+p_out   = 820                               # Compressor outlet pressure [bar] #Gabriele: is your p_out at 820 bar? Please see in the litearture what value is set here, I guess it depends on the pressure you store the hydrogen in the tank;
+p_in    = 50                                # Compressor inlet pressure [bar] #Gabriele: 50 is fine I think, but please check this with Christian. the PEM electrolyzer at Empa works at 30 bar actually
 L_is_C  = k/(k-1)*R_H2*T_in_H2*((p_out/p_in)**((k-1)/k)-1) # Specific work compressor [kJ/kg] 
 
 # Maximal TANK energy capacity (J) => 2 days storage capacity
-S_TANK_max = E_demand_day * 4 * deltat                  # E_demand_day in [Wh]
+S_TANK_max = E_demand_day * 4 * deltat                  # E_demand_day in [Wh] #Gabriele: E_demand_day is in [Wh], correct? why do you multiply for deltat? 
 S_TANK_H2_max = S_TANK_max / HHV                        # equivalent in kg_H2
 
 # Maximal FC size as the maximal power demand divided by eff in [W]
@@ -115,18 +115,23 @@ P_ELY   = PV_sizingprob.addVars(nHours, lb=0, ub=S_ELY_max,    name='P_ELY')
 E_TANK  = PV_sizingprob.addVars(nHours, lb=0, ub=S_TANK_max,   name='E_TANK')
 P_FC    = PV_sizingprob.addVars(nHours, lb=0, ub=S_FC_max,     name='P_FC')    
 P_imp   = PV_sizingprob.addVars(nHours, lb=0, ub=GRB.INFINITY, name="P_imp")   # Imported electricity from the Grid in [W]
-P_exp   = PV_sizingprob.addVars(nHours, lb=0, ub=max(P_ELY),   name="P_exp")   # Check upper-bound
+P_exp   = PV_sizingprob.addVars(nHours, lb=0, ub=max(P_ELY),   name="P_exp")   # Check upper-bound; #gabriele: replace max(P_ELY) with S_ELY. In general, avoid using max() when defining constraints or design variables.
+#Gabriele: In Matlab, we set the ub to the max design value, e.g S_ELY_MAX (or max of demand, which you can extract from the time series), and then we define a constraint like:
+#Gabriele: PV_sizingprob.addConstrs((P_exp[t] <= S_ELY for t in range(nHours)), name="P_exp_upper_bound")
+#Gabriele: I think we already discussed this, but I forgot :) could you explain me again the rationale to limit the exported power to the ely size? I am in favour of limiting the exported power, I just don't remember why we set that as max value
 
 #------------------------------------------------------------------------------
 # Additional variables definition
 # maximum PV size needs to be defined to define the upper bound of the design variable.
 P_PV = [irradiance[t] * eta['PV'] * Area_PV for t in range(nHours)]  # Hourly PV power generation [W]
-P_PV_peak = max(irradiance) * eta['PV'] * Area_PV  # Peak power for investment cost [W]
+P_PV_peak = max(irradiance) * eta['PV'] * Area_PV  # Peak power for investment cost [W] #Gabriele: in optimization problems, try to avoid using the max() function. Besides, the peak power 
+#Gabriele: do you have a reference for the definition of P_PV_peak you adopted? I normally consider the peak power from Standard Test Conditions (STC), for which irradiance is fixed at 1000 W/m2. I think it is fairer, otherwise you have your peak power depending on sun availability, and given that your unit price is CHF/kW, it would not be fair to have an investiment cost depending on sun availability (how much units you install instead depends on the sun availability)
 S_PV = P_PV_peak
 
+
 # Hydrogen mass flow
-# Nominal mass flow hydrogen [kg/s]
-mdot_H2_nom = (S_ELY * eta["ELY"] * deltat) / HHV     # wird nicht ausgerechnet           
+# Nominal mass flow hydrogen [kg/s] #Gabriele: is the flow below kg/s or kg/h?
+mdot_H2_nom = (S_ELY * eta["ELY"] * deltat) / HHV     # wird nicht ausgerechnet       
 # Mass flow produced hydrogen [kg/h]                    
 mdot_H2 = [(P_ELY[t] * eta['ELY'] * deltat) / HHV for t in range(nHours)] 
 
@@ -136,26 +141,26 @@ P_C = [mdot_H2[t] * L_is_C / (eta['C'] * deltat) for t in range(nHours)]
 
 #------------------------------------------------------------------------------
 # Constraints
-PV_sizingprob.addConstr(E_TANK[0] == 0)
+PV_sizingprob.addConstr(E_TANK[0] == 0) #Gabriele: this constraint 'conflicts' with line 158, I would use only line 158 and delete line 142
 
-for t in range(1, nHours):
+for t in range(1, nHours): #Gabriele: as you know, vectors in python starts at index 0, so please make sure you are always imposing conditions also for the initial timestep
     # constraint for the energy balance in time over the storage component
-    PV_sizingprob.addConstr(E_TANK[t] == E_TANK[t-1] + P_ELY[t] * eta['ELY'] * deltat - P_FC[t] * deltat / eta['FC'])
-    # constraint for the ELY power not to excedd the storage capacity
-    PV_sizingprob.addConstr(P_ELY[t]  <= (S_TANK - E_TANK[t-1]) / deltat) 
+    PV_sizingprob.addConstr(E_TANK[t] == E_TANK[t-1] + P_ELY[t] * eta['ELY'] * deltat - P_FC[t] * deltat / eta['FC']) #Gabriele: can you please check in the results that this energy balance is respected? Please check over two/three random timesteps, and also over the year, i.e. integral for PEM power, tank energy and FC power. This should differ by the efficiency terms. 
+    # constraint for the ELY power not to exceed the storage capacity
+    PV_sizingprob.addConstr(P_ELY[t]  <= (S_TANK - E_TANK[t-1]) / deltat) #Gabriele: this might be correct as it is already specified in line 148, but please double check that the efficiency is not needed in this inequality
     # constraint for the FC to only use hydrogen available in the storage
-    PV_sizingprob.addConstr(P_FC[t]   <= E_TANK[t-1] / deltat)
-    PV_sizingprob.addConstr(P_FC[0]   <= E_TANK[0] / deltat) 
+    PV_sizingprob.addConstr(P_FC[t]   <= E_TANK[t-1] / deltat) #Gabriele: same comment as for line 150
+    PV_sizingprob.addConstr(P_FC[0]   <= E_TANK[0] / deltat) #Gabriele: this constraint does not belong to the for loop
     
     PV_sizingprob.addConstr((P_ELY[t]  <= S_ELY),  "upper_Size_Constraint_ELY")
     PV_sizingprob.addConstr((E_TANK[t] <= S_TANK), "upper_Size_Constraint_TANK")
     PV_sizingprob.addConstr((P_FC[t]   <= S_FC ),   "upper_Size_Constraint_FC")
 
 # constraint for H2 storage equal at final and last time step (periodicity)
-PV_sizingprob.addConstr(E_TANK[0] == E_TANK[nHours-1])
+PV_sizingprob.addConstr(E_TANK[0] == E_TANK[nHours-1]) 
 
 # Overall energy balance: left => consumers | right => generators
-PV_sizingprob.addConstrs((P_ELY[t] + P_C[t] + P_exp[t] + P_demand[t] <= P_PV[t] + P_imp[t] + P_FC[t]  for t in range(nHours)), name='EnergyBalance')  
+PV_sizingprob.addConstrs((P_ELY[t] + P_C[t] + P_exp[t] + P_demand[t] <= P_PV[t] + P_imp[t] + P_FC[t]  for t in range(nHours)), name='EnergyBalance') #Gabriele: I see here you added a name for the constraints, while you did not in other lines. Please make sure that a name is not always needed and that constraints are not overwritten if a name is not given.
 
 #------------------------------------------------------------------------------
 # Define cost functions
@@ -173,7 +178,7 @@ for component in components:
 # Operation costs in [€/y]
 cost_op = sum((P_imp[t] * cost_imp_el[t] - P_exp[t] * cost_exp_el[t]) for t in range(nHours))
 
-# Maintenance cost
+# Maintenance cost 
 # cost_maint = 
 
 # Total annual costs in [k€/y]
@@ -186,6 +191,13 @@ PV_sizingprob.setObjective(cost, GRB.MINIMIZE)
 #------------------------------------------------------------------------------
 # Solve optimization problem
 PV_sizingprob.optimize() 
+
+#Gabriele: overall, the code makes sense to me and I did not find any critical mistakes. Please perform these verifications of the results:
+#Gabriele: (i) for a simple energy demand and off-grid scenario, compare your estimations (pen and paper, or Excel) and the code results;
+#Gabriele: (ii) from the code results in terms of size and operation, calculate from an Excel sheet the total cost and compare it with the objective function calculated in the optimization (they have to be equal)
+#Gabriele: (iii) for random timesteps, verify one by one that your constraints are all satisfied. For example, that power balance is correct, that FC power is lower than 'available power' from the tank, etc.
+#Gabriele: (iv) verify the energy balances over the whole year. This can be done by integrating the different power flows. Please be careful to consider the efficiencies. 
+
 
 #------------------------------------------------------------------------------
 # Check the optimization status
@@ -221,15 +233,15 @@ fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, figsize=(20, 10))
 
 # Plot 1: Size and operation of the ELY
 ax1.plot(range(len(df_input)), [P_ELY[t].X / 1000 for t in range(len(df_input))], label='ELY Electrolyzer (kW)', color=palette[1])
-ax1.plot(range(len(df_input)), [S_ELY.X / 1000] * len(df_input), label='ELY size (kW)', linestyle='--', color=palette[3])
+ax1.plot(range(len(df_input)), [S_ELY.X / 1000] * len(df_input), label='ELY size (kW)', linestyle='--', col-or=palette[3])
 ax1.set_xlabel('Time [h]')
 ax1.set_ylabel('Power [kW]')
 ax1.set_ylim([0, S_ELY.X * 1.1 / 1000])
 ax1.legend(loc='upper right')
 
 # Plot 1: Size and operation of the FC
-ax2.plot(range(len(df_input)), [P_FC[t].X / 1000 for t in range(len(df_input))], label='Fuel Cell (kW)', color=palette[0])
-ax2.plot(range(len(df_input)), [S_FC.X / 1000] * len(df_input), label='FC size (kW)', linestyle=':', color=palette[2])
+ax2.plot(range(len(df_input)), [P_FC[t].X / 1000 for t in range(len(df_input))], label='Fuel Cell (kW)', col-or=palette[0])
+ax2.plot(range(len(df_input)), [S_FC.X / 1000] * len(df_input), label='FC size (kW)', linestyle=':', col-or=palette[2])
 ax2.set_xlabel('Time [h]')
 ax2.set_ylabel('Power [kW]')
 ax2.set_ylim([0, S_FC.X * 1.1 / 1000])
