14a77265e64b56d5a8d1595515c409d426ce72f9
mfischer030 <108004040+mfischer030@users.noreply.github.com>
2024-03-07 13:23:17 +0100

    Add files via upload

---
+++ PSWHR/functions/cost.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 17:14:39 2024
+
+@author: fism
+
+"""
+import pandas as pd
+import numpy as np
+
+def totalAnnualCost(system_sizes, energy_tariff,
+                    UP, maintenance, life, 
+                    P_imp, P_max_imp, P_exp, P_th_LT, P_th_HT,
+                    cost_imp_el, cost_exp_el, cost_export_heatLT, cost_export_heatHT,
+                    df_input, nHours, timeline_choice):
+    
+    # Capital recovery factor
+    # CRF = (annual_interest_rate * (1 + annual_interest_rate)**project_lifetime) / ((1 + annual_interest_rate)**project_lifetime - 1) # not used
+
+    # Installation and maintenance costs in [€/y]------------------------------
+    cost_inst  = 0  # Initialize cost_inst outside the loop
+    cost_maint = 0
+    
+    cost_inst  = sum((size * UP[component]) / life[component] for component, size in system_sizes.items())
+    cost_maint = sum(size * UP[component] * maintenance[component] for component, size in system_sizes.items())
+
+    # # Operation costs in [€/y]-------------------------------------------------
+    # # Defining multiplier variable für OP cost for choosen timeline
+    # if timeline_choice == 'week':
+    #     multiplier = 52                               # Assuming 52 weeks in a year
+    # elif timeline_choice == 'month':
+    #     multiplier = 12                               # 12 months in a year
+    # else:
+    #     multiplier = 1                                # Default to yearly calculation with no multiplication needed
+
+    # cost_energy    = sum((P_imp[t]/10**6 * cost_imp_el[t] - P_exp[t]/10**6 * cost_exp_el[t]) for t in range(nHours)) * multiplier
+    # # cost_griduse = 12*P_max_imp*16
+    # cost_griduse   = sum(((P_imp[t] + P_exp[t]) / 10**3) * (2.01 + 5.75)/100  for t in range(nHours)) * multiplier
+    # cost_WHR       = sum((P_th_LT[t]/1000 * cost_export_heatLT + P_th_HT[t]/1000 * cost_export_heatHT) for t in range(nHours))
+
+    # cost_op = cost_energy + cost_griduse - cost_WHR
+    
+    # Operation costs in [€/y]-------------------------------------------------
+    def electricity_prices(energy_tariff, P_imp, P_max_imp, P_exp, df_input, timeline_choice, vat_included=False):
+        
+     
+        
+        # Calculate the mean operating time (mittlere Benutzungsdauer)
+        if timeline_choice == 'week':
+            multiplier = 52                               # Assuming 52 weeks in a year
+        elif timeline_choice == 'month':
+            multiplier = 12                               # 12 months in a year
+        else:
+            multiplier = 1                                # Default to yearly calculation with no multiplication needed
+        
+        # Calculate the mean of the monthly maximum values
+        mean_monthly_max = 0 
+        for month in P_max_imp: 
+            mean_monthly_max += P_max_imp[month] / len(P_max_imp)
+        
+        print(mean_monthly_max)
+        avr_operating_time = P_imp.sum() / mean_monthly_max
+        
+        #avr_operating_time = 3600
+        
+        # print(f"The monthly maximum values is: {sum(P_imp)}")
+        # print(f"The mean of the monthly maximum values is: {mean_monthly_max}")
+        # print(f"The average operating time is: {avr_operating_time}")
+        
+        # Base fees in CHF for choosen timeline (week,month,year)
+        base_fee_incl_vat = (42.16 + 616.17) / multiplier
+        base_fee_excl_vat = (39.00 + 570.00) / multiplier
+
+        # Tariff-specific prices in Rappen per kWh
+        tariffs = {
+            "Green": {"peak_excl_vat": 14.92, "peak_incl_vat": 16.13, "off_peak_excl_vat": 12.12, "off_peak_incl_vat": 13.10},
+            "Blue":  {"peak_excl_vat": 12.42, "peak_incl_vat": 13.43, "off_peak_excl_vat": 9.62, "off_peak_incl_vat": 10.40},
+            "Grey":  {"peak_excl_vat": 11.42, "peak_incl_vat": 12.35, "off_peak_excl_vat": 8.62, "off_peak_incl_vat": 9.32},
+        }
+        
+        if avr_operating_time > 3500: 
+            grid_usage_perkWh = {
+                "Leistungstarif": {"excl_vat": 15.99, "incl_vat": 17.29},
+                "Hochtarif":   {"excl_vat": 4.03, "incl_vat": 4.36},
+                "Niedertarif": {"excl_vat": 2.01, "incl_vat": 2.17},
+                }
+        else:
+            grid_usage_perkWh = { 
+                "Leistungstarif": {"excl_vat": 7.55, "incl_vat": 8.16},
+                "Hochtarif":   {"excl_vat": 7.47, "incl_vat": 8.08},
+                "Niedertarif": {"excl_vat": 3.74, "incl_vat": 4.04},
+                }
+        
+        # additionnal_grid_fees = {
+        #     "SystemServices_Swissgrid" : {"excl_MWSt": 0.75, "incl_vat": 0.81},
+        #     "Stromreserve"             : {"excl_MWSt": 1.20, "incl_vat": 1.30},
+        #     "Gesetzliche_Foerderabgabe": {"excl_MWSt": 2.30, "incl_vat": 2.49},
+        #     "Abgabe_an_die_Gemeinde"   : {"excl_MWSt": 1.50, "incl_vat": 1.62},
+        #     }
+        additionnal_grid_fees = {
+            "excl_vat": 0.75 + 1.20 + 2.30 + 1.50, 
+            "incl_vat" : 0.81 + 1.30 + 2.49 + 1.62
+            }
+
+        if energy_tariff not in tariffs:
+            raise ValueError("Invalid tariff. Choose Green, Blue, or Grey.")
+
+        energy_tariff_choice = tariffs[energy_tariff]
+        
+        # Given prices for exported energy
+        price_export_energy = [13.07, 7.73, 7.24, 8.66]
+
+        # Calculate the mean of these prices using numpy
+        price_export_energy = np.mean(price_export_energy)
+        
+        # Peak hours definition
+        peak_time_start = 7
+        peak_time_end = 21
+
+        # Initializing costs
+        cost_elec_imp = 0
+        cost_elec_exp = 0 
+        cost_elec     = base_fee_incl_vat if vat_included else base_fee_excl_vat
+        
+        for i, row in df_input.iterrows():
+            hour = row['HR']
+            if peak_time_start <= hour < peak_time_end:
+                price_energy_per_kWh = energy_tariff_choice["peak_incl_vat" if vat_included else "peak_excl_vat"]
+                price_grid_per_kWh = grid_usage_perkWh["Hochtarif"]["incl_vat" if vat_included else "excl_vat"]
+                price_grid_per_kWh += additionnal_grid_fees["incl_vat" if vat_included else "excl_vat"] 
+            else:
+                price_energy_per_kWh = energy_tariff_choice["off_peak_incl_vat" if vat_included else "off_peak_excl_vat"]
+                price_grid_per_kWh = grid_usage_perkWh["Niedertarif"]["incl_vat" if vat_included else "excl_vat"]
+                price_grid_per_kWh += additionnal_grid_fees["incl_vat" if vat_included else "excl_vat"]
+            
+            cost_elec_imp += P_imp[i] / 1000 * price_energy_per_kWh / 100       # in € assuming 1CHF = 1€
+            cost_elec_exp += P_exp[i] / 1000 * price_export_energy / 100        # in €
+        
+        # Calculating the grid fees associated with imports (Netzentgelte)
+        sum_P_imp = 0
+        sum_P_imp += sum(P_imp[t] for t in range(nHours))
+        cost_grid_usage = sum_P_imp / 1000 * price_grid_per_kWh / 100 
+        
+        for month in P_max_imp:
+            cost_grid_usage += P_max_imp[month] / 1000 * grid_usage_perkWh["Leistungstarif"]["incl_vat" if vat_included else "excl_vat"]
+        
+        # Total cost of electricity imports and exports + grid fees
+        cost_elec += (cost_elec_imp - cost_elec_exp) + cost_grid_usage
+
+        return cost_elec, cost_elec_imp, cost_elec_exp, cost_grid_usage
+    
+    cost_elec, cost_elec_imp, cost_elec_exp, cost_grid_usage = electricity_prices(energy_tariff, P_imp, P_max_imp, P_exp, df_input, timeline_choice, vat_included=False)
+    
+    cost_WHR = sum((P_th_LT[t]/1000 * cost_export_heatLT + P_th_HT[t]/1000 * cost_export_heatHT) for t in range(nHours))
+    
+    cost_op  = cost_elec - cost_WHR
+
+    return cost_inst, cost_elec_imp, cost_elec_exp, cost_grid_usage, cost_elec, cost_op, cost_maint, cost_WHR
+
+# Code from Roxanne------------------------------------------------------------
+
+# cost without WHR
+# cost_inst = (P_PV_peak*UP_PV/life_PV+P_e_nom*UP_e/life_e+C_b/3600*UP_b/life_b)/1000 # k€
+# cost_op   = sum(P_imp.*cost_el)/1000 # k€
+
+# costs with WHR
+# cost_inst    = (P_PV_peak * UP_PV/life_PV + P_e_nom * UP_e/life_e + C_b/3600 * UP_b/life_b + S_HP * UP_HP/life_HP + 43872*(S_c/1000)^(0.5861)/life_c + UP_storage * mass_H2_day_obj/life_storage)/1000; # [kEUR]
+# cost_op      = sum(P_imp.*cost_el)/1000 - sum(P_th_HT.*cost_export_heatHT)/1000 - sum(P_exp.*cost_export_el)/1000 # [kEUR]
+# cost_startup = sum(startup*cost_startup_ely)/1000
+
+# costs with more WHR and maintenance
+# cost_inst    = (P_PV_peak * UP_PV * ann_PV + P_e_nom * UP_e * ann_e + C_b/3600 * UP_b * ann_b + S_HP * UP_HP * ann_HP + (A_HEX*UP_HEX + Fixed_HEX) * ann_HEX + 43872*(S_c)^(0.5861) * ann_c + UP_storage * mass_H2_day_obj * ann_storage + UP_disp)/1000; # [kEUR/y] add more components minutillo, dispenser and refrigeration, size known by size storage
+# cost_inst    = (P_PV_peak * UP_PV * ann_PV + cost_e * ann_e + C_b/3600 * UP_b * ann_b + S_HP * UP_HP * ann_HP + (A_HEX*UP_HEX + Fixed_HEX) * ann_HEX + cost_c * ann_c + UP_storage * mass_H2_day_obj * ann_storage + UP_disp + P_refr * UP_refr * ann_refr )/1000; # [kEUR/y] add more components minutillo, dispenser and refrigeration
+# cost_op      = sum([P_imp[i] * cost_el[i] for i in range(len(P_imp))]) / 1000 - sum([P_th_HT[i] * cost_export_heatHT[i] for i in range(len(P_th_HT))]) / 1000 - sum([P_th_LT[i] * cost_export_heatLT[i] for i in range(len(P_th_LT))]) / 1000 - sum([P_exp[i] * cost_export_el[i] for i in range(len(P_exp))]) / 1000                         # [kEUR/y]
+# cost_maint   = (maint_PV * P_PV_peak * UP_PV + maint_e * cost_e + maint_b * C_b / 3600 * UP_b * ann_b + maint_HP * S_HP * UP_HP * ann_HP + maint_HEX * (A_HEX * UP_HEX + Fixed_HEX) * ann_HEX + maint_c * cost_c + maint_storage * UP_storage * mass_H2_day_obj * ann_storage + maint_disp * UP_disp + maint_refr * P_refr * UP_refr) / 1000  # [kEUR/y] mainly Poalo Gabrielli data
+# cost_startup = sum([startup[i] * cost_startup_ely[i] for i in range(len(startup))]) / 1000
+#------------------------------------------------------------------------------
---
+++ PSWHR/functions/plotting_module.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 15:25:19 2024
+
+@author: fism
+"""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set Seaborn style
+sns.set(style="whitegrid")
+palette = sns.color_palette("husl", 8)  # You can choose a different palette if needed
+
+# Increase font sizes for better readability on PowerPoint slides
+plt.rcParams.update({'font.size': 24})  # Adjust global font size
+plt.rcParams['axes.labelsize'] = 24
+plt.rcParams['axes.titlesize'] = 24
+plt.rcParams['xtick.labelsize'] = 24
+plt.rcParams['ytick.labelsize'] = 24
+plt.rcParams['legend.fontsize'] = 24
+
+def heat_demand_plot(heat_35degC_demand,heat_65degC_demand):
+    # Creating figure and subplots for heat demand
+    fig, axs = plt.subplots(2, 1, figsize=(20, 10), sharex=True)
+    fig.suptitle('Heat Demand 2022 NEST', fontsize=28)
+
+    # Plot Heating demand at 35°C
+    axs[0].fill_between(range(len(heat_35degC_demand)), heat_35degC_demand, label='Heating Demand 35°C', color='orange', alpha=0.4)
+    axs[0].set_ylabel('Demand (kW)', fontsize=24)
+    axs[0].legend(loc='upper right', fontsize=24)
+
+    # Plot DHW demand at 65°C
+    axs[1].fill_between(range(len(heat_65degC_demand)), heat_65degC_demand, label='Heat Demand 65°C', color='red', alpha=0.4)
+    axs[1].set_ylabel('Demand (kW)', fontsize=24)
+    axs[1].legend(loc='upper right', fontsize=24)
+
+    # Setting the xlabel for the last subplot
+    axs[1].set_xlabel('Time in Hours', fontsize=24)
+
+    plt.tight_layout()
+    plt.show()
+
+# def plot_power_generation(P_PV, P_imp, P_exp, df_input):
+#     # Plot power generation, imported, and exported power
+#     fig, ax = plt.subplots(1, 1, figsize=(20, 10))  # Use a single axis
+#     ax.plot(range(len(df_input)), [P_PV[t]  / 1000 for t in range(len(df_input))], label='PV Generation', color='orange')
+#     ax.plot(range(len(df_input)), [P_imp[t] / 1000 for t in range(len(df_input))], label='Imported Power', color='blue')
+#     # Plot P_exp as negative values
+#     ax.plot(range(len(df_input)), [-P_exp[t] / 1000 for t in range(len(df_input))], label='Exported Power', color='green')
+    
+#     ax.set_ylabel('Power [kW]')
+#     ax.legend(loc='upper left')
+#     ax.set_title('PV Generation, Imported, and Exported Power')
+#     ax.set_xlabel('Time [h]')  # Set the x-label here
+
+#     plt.show()
+    
+def plot_power_generation(P_PV, P_imp, P_exp, df_input):
+    # Plot power generation, imported, and exported power
+    fig, ax = plt.subplots(1, 1, figsize=(20, 6))  # Use a single axis with a larger figsize for readability
+    
+    # Plot and fill PV Generation
+    ax.fill_between(range(len(df_input)), [P_PV[t] / 1000 for t in range(len(df_input))], color='orange', alpha=0.3)
+    ax.plot(range(len(df_input)), [P_PV[t] / 1000 for t in range(len(df_input))], label='PV Generation', color='orange')
+    
+    # Plot and fill Imported Power
+    ax.fill_between(range(len(df_input)), [P_imp[t] / 1000 for t in range(len(df_input))], color='magenta', alpha=0.3)
+    ax.plot(range(len(df_input)), [P_imp[t] / 1000 for t in range(len(df_input))], label='Imported Power', color='magenta')
+    
+    # Plot and fill Exported Power as negative values
+    ax.fill_between(range(len(df_input)), [-P_exp[t] / 1000 for t in range(len(df_input))], color='green', alpha=0.3)
+    ax.plot(range(len(df_input)), [-P_exp[t] / 1000 for t in range(len(df_input))], label='Exported Power', color='green')
+    
+    ax.set_ylabel('Power [kW]')  # Increase the font size for the y-axis label
+    ax.set_xlabel('Time [h]')  # Increase the font size for the x-axis label
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.17),
+          fancybox=False, shadow=False, ncol=5)
+    #ax.legend(loc='upper right')  # Increase the font size for the legend
+    #ax.set_title('PV Generation, Imported, and Exported Power')  
+    
+    # Increase the font size for the tick labels
+    ax.tick_params(axis='both', which='major')
+    
+    plt.show()
+
+
+# def plot_component_sizes(S_PV, S_PV_max, S_ELY, S_FC, S_TANK, S_ELY_max, S_FC_max, S_TANK_max):
+#     # Plot sizes of components (PV, ELY, FC, TANK)
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 8))
+    
+#     # Integrating S_PV and S_PV_max into the first subplot (now ax1)
+#     components_axis1 = ['S_PV', 'S_PV_max', 'S_ELY', 'S_ELY_max', 'S_FC', 'S_FC_max']
+#     component_sizes_axis1 = [S_PV /1000, S_PV_max / 1000, S_ELY / 1000, S_ELY_max / 1000, S_FC / 1000, S_FC_max / 1000]
+#     colors_axis1 = ['mediumturquoise', 'aquamarine', 'blue', 'lightblue', 'green', 'lightgreen']
+    
+#     ax1.bar(components_axis1, component_sizes_axis1, color=colors_axis1, width=0.7)
+#     ax1.set_ylabel('Size (Kilowatts)')
+    
+#     # Second subplot (now ax2) remains unchanged, for TANK sizes
+#     components_axis2 = ['S_TANK', 'S_TANK_max']
+#     component_sizes_axis2 = [S_TANK / 3600000, S_TANK_max / 3600000]  # Converting to Kilowatt-hours
+#     colors_axis2 = ['orange', 'lightcoral']
+    
+#     ax2.bar(components_axis2, component_sizes_axis2, color=colors_axis2, width=0.7)
+#     ax2.set_ylabel('Size (Kilowatt-hours)')
+
+#     plt.tight_layout()
+#     plt.show()
+
+def plot_component_sizes(S_PV, S_PV_max, S_ELY, S_ELY_max, S_C, S_C_max, S_FC, S_FC_max, S_TANK, S_TANK_max):
+    # Setting the Seaborn color palette
+    sns.set_theme(style="whitegrid")
+    colors = sns.color_palette("deep", 5)  # Using the 'deep' palette for 5 different colors
+
+    # First plot for S_PV, S_ELY, S_C, S_FC
+    fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(10, 8))
+    components_kw = ['S_PV', 'S_ELY', 'S_C', 'S_FC']
+    component_sizes_kw = [S_PV / 1000, S_ELY / 1000, S_C /1000, S_FC / 1000]  # Converting to kW
+    max_values_kw = [S_PV_max / 1000, S_ELY_max / 1000, S_C_max / 1000, S_FC_max / 1000]
+    
+    ax0.bar(components_kw, component_sizes_kw, color=colors[:4], width=0.4)
+    for i, max_val in enumerate(max_values_kw):
+        ax0.axhline(y=max_val, color=colors[i], linestyle='--', label=f'{components_kw[i]} Max')
+
+    ax0.set_ylabel('Size (Kilowatts)', fontsize=24)
+    ax0.tick_params(axis='both', which='major', labelsize=24)
+    
+    # Second plot for S_TANK
+    components_MWh = ['S_TANK']
+    component_sizes_MWh = [S_TANK / (3600000*1000)]  # Converting to MWh
+    color_MWh = colors[4]  # Using the fifth color for S_TANK
+
+    ax1.bar(components_MWh, component_sizes_MWh, color=color_MWh, width=0.4)
+    ax1.axhline(y=S_TANK_max / (3600000*1000), color=color_MWh, linestyle='--', label='S_TANK Max')
+
+    ax1.set_ylabel('Size (Megawatt-hours)', fontsize=24)
+    ax1.tick_params(axis='both', which='major', labelsize=24)
+
+    plt.tight_layout()
+    plt.show()
+    
+# def plot_component_sizes(Area_PV, Area_PV_max, S_ELY, S_FC, S_TANK, S_ELY_max, S_FC_max, S_TANK_max):
+#     # Plot sizes of components (ELY, FC, TANK)
+#     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+    
+#     components_axis1 = ['Area_PV', 'Area_PV_max']
+#     component_sizes_axis1 = [Area_PV, Area_PV_max]
+#     colors_axis1 = ['mediumturquoise', 'aquamarine']
+
+#     ax1.bar(components_axis1, component_sizes_axis1, color=colors_axis1, width=0.7)
+#     #ax1.set_xlabel('Components')
+#     ax1.set_ylabel('Size (Squaremeters)')
+    
+#     components_axis2 = ['S_ELY', 'S_ELY_max', 'S_FC', 'S_FC_max']
+#     component_sizes_axis2 = [S_ELY / 1000, S_ELY_max / 1000, S_FC / 1000, S_FC_max / 1000]
+#     colors_axis2 = ['blue', 'lightblue', 'green', 'lightgreen']
+    
+#     ax2.bar(components_axis2, component_sizes_axis2, color=colors_axis2, width=0.7)
+#     #ax2.set_xlabel('Components')
+#     ax2.set_ylabel('Size (Kilowatts)')
+
+#     components_axis3 = ['S_TANK', 'S_TANK_max']
+#     component_sizes_axis3 = [S_TANK / 3600000, S_TANK_max / 3600000]
+#     colors_axis3 = ['orange', 'lightcoral']
+
+#     ax3.bar(components_axis3, component_sizes_axis3, color=colors_axis3, width=0.7)
+#     #ax3.set_xlabel('Components')
+#     ax3.set_ylabel('Size (Kilowatt-hours)')
+
+#     plt.tight_layout()
+#     plt.show()
+
+
+def plot_HESS_results(P_PV, P_ELY, S_ELY, S_ELY_max, P_FC, S_FC, S_FC_max, E_TANK, S_TANK, S_TANK_max, df_input):
+    """
+    Plot main results including the size and operation of the electrolyzer (ELY),
+    fuel cell (FC), and energy and TANK size over time.
+
+    Parameters:
+    - P_PV: list, hourly PV power generation values
+    - P_ELY: list, hourly electrolyzer power values
+    - S_ELY: float, electrolyzer size
+    - P_FC: list, hourly fuel cell power values
+    - S_FC: float, fuel cell size
+    - E_TANK: list, hourly tank energy values
+    - S_TANK: float, tank size
+    - S_TANK_max: float, maximal tank size
+    - df_input: DataFrame, input data
+    """
+    # Plotting
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, figsize=(20, 10))
+
+    # Plot 1: Size and operation of the ELY
+    ax1.plot(range(len(df_input)), [P_ELY[t] / 1000 for t in range(len(df_input))], label='ELY', color=palette[1])
+    ax1.plot(range(len(df_input)), [S_ELY / 1000] * len(df_input), label='ELY size', linestyle='--', color=palette[3])
+    # ax1.set_xlabel('Time [h]')
+    ax1.set_ylabel('Power [kW]')
+    # ax1.set_ylim([0, S_ELY_max * 1.1 / 1000])
+    ax1.set_ylim([0, 600])
+    ax1.legend(loc='upper right')
+
+    # Plot 2: Size and operation of the FC
+    ax2.plot(range(len(df_input)), [P_FC[t] / 1000 for t in range(len(df_input))], label='FC', color=palette[0])
+    ax2.plot(range(len(df_input)), [S_FC / 1000] * len(df_input), label='FC size', linestyle=':', color=palette[2])
+    # ax2.set_xlabel('Time [h]')
+    ax2.set_ylabel('Power [kW]')
+    # ax2.set_ylim([0, S_FC_max * 1.1 / 1000])
+    ax2.set_ylim([0, 500])
+    ax2.legend(loc='upper right')
+
+    # Plot 3: Energy and TANK Size
+    ax3.plot(range(len(df_input)), [E_TANK[t] / 3600000 for t in range(len(df_input))], label='TANK', color=palette[5])
+    ax3.plot(range(len(df_input)), [S_TANK / 3600000] * len(df_input), label='TANK size', linestyle='-.', color=palette[6])
+    ax3.set_xlabel('Time [h]')
+    ax3.set_ylabel('Energy [kWh]')
+    # ax3.set_ylim([0, (S_TANK_max * 1.1) / 3600000])
+    ax3.set_ylim([0, 5000])
+    ax3.legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_costs_and_prices(cost_inst, cost_op, cost, cost_maint, df_input):
+    """
+    Plot installation cost, operation cost, total cost, and electricity prices.
+
+    Parameters:
+    - cost_inst: LinExpr object of gurobipy module, installation cost
+    - cost_op: LinExpr object of gurobipy module, operation cost
+    - cost: LinExpr object of gurobipy module, total cost
+    - cost_main: LinExpr object of gurobipy module, main cost
+    - cost_startup: LinExpr object of gurobipy module, startup cost
+    - df_input: DataFrame, input data
+    """
+
+    # Set Seaborn style
+    sns.set(style="whitegrid")
+    palette = sns.color_palette("pastel")
+
+    # Plotting
+    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(15, 15))
+
+    # Plot electricity prices
+    ax1.plot(range(len(df_input)), df_input['price_Eur_MWh'], label='Import Price', color=palette[0])
+    ax1.plot(range(len(df_input)), df_input['Price_DayAhed'], label='Export Price', color=palette[1])
+    ax1.set_xlabel('Time [h]', fontsize=24)
+    ax1.set_ylabel('Price [€/Wh]', fontsize=28)
+    ax1.legend(fontsize=24)
+    
+    # Increase the font size for the tick labels
+    ax1.tick_params(axis='both', which='major', labelsize=24)
+
+    # Plot installation cost, operation cost, main cost, startup cost, and total cost
+    bar_width = 0.4
+    bar1 = ax2.bar(0, cost_inst / 1000, bar_width, label='Installation Cost', color=palette[2])
+    bar2 = ax2.bar(1, cost_op   / 1000, bar_width, label='Operation Cost', color=palette[3])
+    bar3 = ax2.bar(2, cost_maint / 1000, bar_width, label='Main Cost', color=palette[4])
+    bar4 = ax2.bar(3, cost      / 1000, bar_width, label='Total Cost', color=palette[6])
+
+    ax2.set_ylabel('Cost [k€/y]', fontsize=28)
+    ax2.set_xticks([0, 1, 2, 3])
+    ax2.set_xticklabels(['Installation Cost', 'Operation Cost', 'Maintenance Cost', 'Total Cost'], fontsize=24)
+    ax2.tick_params(axis='both', which='major', labelsize=24)  # Set tick label font size for ax2
+
+    plt.tight_layout()
+    plt.show()
+
